42a2b860998c4b2af9b3f6673dd2b3a9ccae10c1
FabiaEjaz <fabiaejaz316@gmail.com>
2024-11-17 04:25:26 -0800

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int a = 5;
+int b = 10;
+int main()
+{
+    cout << "The value of a is:" << a << endl;
+    cout << "The value of b is:" << b << endl;
+    return 0;
+}
